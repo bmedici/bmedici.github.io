a708c0a61d8a3e4ae832fcff721087deb3f561e0
bruno <bruno@ted.local>
2018-07-12 17:55:25 +0200

    Change cv generator

--- helpers/custom_helpers.rb
+++ helpers/custom_helpers.rb
@@ -14,18 +14,34 @@ module CustomHelpers
 
   def localized(entry)
     loc = I18n.locale
+    html = []
 
+    # We have no translation
     if !entry.is_a? Hash
-      return content_tag(:span, entry.to_s, style: 'color: blue')
+      html << localized_block(:blue, entry.to_s)
 
+    # We have a translation with many items
+    elsif entry[loc].is_a?(Array)
+      entry[loc].each do |line|
+        html << localized_block(:YELLOWGREEN, line)
+      end
+
+    # We have a siple translation
     elsif value = entry[loc]
-      return content_tag(:span, value, style: 'color: green')
+      html << localized_block(:green, value)
 
+    # Otherwise, as it's a hash with no "loc" version, take the first one 
     elsif value = entry.first
-      return content_tag(:span, value, style: 'color: orange')
+      html << localized_block(:orange, value)
 
     end
 
+    # Build tags
+    return html.join
+  end
+
+  def localized_block color, content
+    content_tag(:div, content.to_s, style: "color: #{color}")
   end
 
 end
