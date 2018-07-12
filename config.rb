d774fd0b5cb0bc4c96d96a3862ded5ac0dd6267b
bruno <bruno@ted.local>
2018-07-12 17:56:36 +0200

    add 404 error page, disable directory indexes

--- config.rb
+++ config.rb
@@ -6,7 +6,7 @@ page "CNAME", layout: false
 
 # Automatic image dimensions on image_tag helper
 activate :automatic_image_sizes
-activate :directory_indexes
+# activate :directory_indexes
 activate :i18n, :mount_at_root => :fr
 
 activate :s3_sync do |s3_sync|
