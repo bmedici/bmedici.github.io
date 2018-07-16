4fed12eba9c50a29cc04574deb5c4ffec7f9de47
Bruno Medici <bm.git@bmconseil.com>
2018-07-16 19:01:03 +0200

    WIP

--- config.rb
+++ config.rb
@@ -113,7 +113,12 @@ configure :build do
 
   # Build PDF files
   activate :pdfkit do |p|
+    p.filenames = {
+      'cv-bruno-medici-fr/index' => 'cv-bruno-medici-fr.pdf',
+      'cv-bruno-medici-en/index' => 'cv-bruno-medici-en.pdf',
+     }
     # p.filenames = ['cv-bruno-medici-fr/index', 'cv-bruno-medici-en/index']
+    p.disable_smart_shrinking = true
     p.quiet = false
     p.page_size = 'A4'
     p.margin_top = 10
@@ -164,6 +169,31 @@ helpers do
   def link_to_locale(text, target, lang=::I18n.locale)
     url = extensions[:i18n].localized_path(target, lang)
     url ? super(text, url) : super(text, target)
+  end
+
+  def get_content(page = current_page)
+    # if page.data.layout == 'orbit'
+    page.render({layout: :orbit})
+  end
+
+  def pdf_for(pagename)
+    page = sitemap.find_resource_by_path(pagename)
+    pdf_for_debug pagename, "found: #{page.class}"
+    pdf_for_debug pagename, "url: #{page.url}"
+
+    html = get_content(page)
+    pdf_for_debug pagename, "html: #{html.bytesize}"
+
+    kit = ::PDFKit.new(html)
+    # kit = ::PDFKit.new('<html><body>HELLO</body></html>')
+    pdf = kit.to_pdf
+    pdf_for_debug pagename, "pdf: #{pdf.bytesize}"
+
+    return pdf
+  end
+
+  def pdf_for_debug(pagename, message)   
+    puts "pdf_for [#{pagename}] #{message}"
   end
 
 end
